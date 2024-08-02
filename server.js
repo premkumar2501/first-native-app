6765595b51004e910a40ab807a69314695cc7729
premkumar2501 <163798437+premkumar2501@users.noreply.github.com>
2024-08-02 10:53:42 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+
+
+import express from 'express'
+
+const app = express()
+app.get('/', (req,res) => {
+    res.send('hlo world')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, (err) => {
+    if(err) console.error('Listen failure', err);
+    console.log('Listen successfully.');
+})
